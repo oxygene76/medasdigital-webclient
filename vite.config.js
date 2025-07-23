5272d351fadee2665225ddbcff7015d9e92077bf
MedasDigital <111655819+oxygene76@users.noreply.github.com>
2025-07-23 17:56:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,97 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import inject from '@rollup/plugin-inject';
+import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
   plugins: [
     nodeResolve({
       preferBuiltins: false,
-      browser: true
+      browser: true,
+      dedupe: ['@cosmjs/stargate', '@cosmjs/proto-signing', 'cosmjs-types']
+    }),
+    commonjs({
+      include: ['node_modules/**'],
+      transformMixedEsModules: true
+    }),
+    inject({
+      global: ['node_modules/buffer/index.js', 'Buffer'],
+      process: 'process/browser'
+    }),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      'global.': 'globalThis.',
+      'Buffer.': 'globalThis.Buffer.'
     })
   ],
   define: {
     global: 'globalThis',
+    'process.env': {},
+    'process.env.NODE_ENV': JSON.stringify('development')
   },
   resolve: {
     alias: {
-      buffer: 'buffer',
-      process: 'process/browser',
-      stream: 'stream-browserify',
-      util: 'util'
+      'crypto': 'crypto-browserify',
+      'stream': 'stream-browserify',
+      'assert': 'assert',
+      'http': 'stream-http',
+      'https': 'https-browserify',
+      'os': 'os-browserify',
+      'url': 'url',
+      'util': 'util',
+      'events': 'events',
+      'path': 'path-browserify',
+      'buffer': 'buffer',
+      'process': 'process/browser'
     }
   },
   optimizeDeps: {
     include: [
       '@cosmjs/stargate',
-      '@cosmjs/proto-signing', 
+      '@cosmjs/proto-signing',
+      '@cosmjs/amino',
+      '@cosmjs/encoding',
+      '@cosmjs/math',
+      '@cosmjs/utils',
       'cosmjs-types',
-      'buffer'
-    ]
+      'buffer',
+      'process/browser',
+      'crypto-browserify',
+      'stream-browserify',
+      'util',
+      'events'
+    ],
+    exclude: []
   },
   server: {
     port: 3000,
-    host: true
+    host: true,
+    cors: true,
+    fs: {
+      allow: ['..']
+    }
   },
   build: {
     target: 'es2020',
+    sourcemap: true,
     rollupOptions: {
       external: [],
       output: {
         manualChunks: {
-          'cosmjs': ['@cosmjs/stargate', '@cosmjs/proto-signing'],
-          'vendor': ['buffer']
+          'cosmjs-core': ['@cosmjs/stargate', '@cosmjs/proto-signing'],
+          'cosmjs-utils': ['@cosmjs/amino', '@cosmjs/encoding', '@cosmjs/math', '@cosmjs/utils'],
+          'cosmjs-types': ['cosmjs-types'],
+          'polyfills': ['buffer', 'process', 'crypto-browserify', 'stream-browserify'],
+          'vendor': ['util', 'events', 'assert']
         }
       }
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
-  }
+  },
+  assetsInclude: ['**/*.wasm']
 });
